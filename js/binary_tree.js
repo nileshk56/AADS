fa63a41dd9abe04cb9347f1c2400b14cc3d2c910
Nilesh Kangane <nilesh.kangane56@gmail.com>
2016-12-20 23:37:36 +0530

    added height function

--- js/binary_tree.js
+++ js/binary_tree.js
@@ -2,7 +2,24 @@ function Node(data){
 	this.data = data;
 	this.left = null;
 	this.right = null;
+};
+
+function height(root){
+	if(root == null)
+		return 0;
+
+	var lHeight = height(root.left);
+	var rHeight = height(root.right);
+
+	return Math.max(lHeight, rHeight) + 1;
 }
 
 
-var root = new Node();
+var root = new Node(56);
+root.left = new Node(1);
+root.right = new Node(2);
+root.left.left = new Node(3);
+root.left.left.left = new Node(4);
+
+
+console.log(height(root));
