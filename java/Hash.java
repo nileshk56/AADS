4a6579703a85b665499011f64f3f99fdc049c40a
Nilesh Kangane <nilesh.kangane56@gmail.com>
2017-03-20 21:04:55 +0530

    more code

--- java/Hash.java
+++ java/Hash.java
@@ -14,7 +14,26 @@ public class Hash
 		{
 			System.out.println(m.getKey() + " " + m.getValue());
 		}
-
 		System.out.println(hm.get(56));
+
+		wordCount("my name is nilesh nilesh is very smart");
+	}
+
+	public static void wordCount(String str) 
+	{
+		String[] arr = str.split(" ");
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		for(String word:arr)
+		{
+			if(!map.containsKey(word))
+				map.put(word, 1);
+			else 
+				map.put(word, map.get(word) + 1);
+		}
+
+		for(String word:arr)
+		{
+			System.out.println(word + " " + map.get(word));
+		}
 	}
 }
