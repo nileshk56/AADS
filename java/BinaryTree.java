5dd7c1c1969d7f28b86a4122a9085112131c9ad2
Nilesh Kangane <nilesh.kangane56@gmail.com>
2017-04-12 02:23:28 +0530

    added height

--- java/BinaryTree.java
+++ java/BinaryTree.java
@@ -1,31 +1,60 @@
 public class BinaryTree
 {
-
-	public class Node()
+	public Node root;
+	public class Node
 	{
 		int data;
 		Node left;
 		Node right;
-
-		//only used for connect nodes at same level functionality
-		Node nextRight;
 
 		public Node(int d)
 		{
 			data = d;
 			left = null;
 			right = null;
-			nextRight = null
 		}
 	}
 
-	public static void connectAllNodesAtSameLevel(Node root)
-	{
+	public int height(Node root){
+		if(root == null) {
+			return 0;
+		}
+
+		int lh = height(root.left);
+		int rh = height(root.right);
+
+		return 1 + Math.max(lh, rh);
+	}
+
+	public int diameter(Node root){
+		if(root == null) {
+			return 0;
+		}
+
+		int lh = height(root.left);
+		int rh = height(root.right);
+
+		mainDiameter = 1 + lh + rh;
+
+
 
 	}
 
+		
+	
+
 	public static void main(String[] args)
 	{
+		BinaryTree bt = new BinaryTree();
 
+		bt.root =  bt.new Node(5);
+		bt.root.left = bt.new Node(1);
+		bt.root.right = bt.new Node(4);
+		bt.root.left.left = bt.new Node(3);
+		bt.root.left.right = bt.new Node(7);
+		bt.root.left.left.left = bt.new Node(2);
+
+		System.out.println(bt.height(bt.root));
+		System.out.println("aaadsfa");
 	}
 }
